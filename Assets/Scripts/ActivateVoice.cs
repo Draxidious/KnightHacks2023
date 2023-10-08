50e48caae94891c889a838b27e0f4ec44cbaf73f
Jordan Muniz <jordy.valois@gmail.com>
2023-10-07 21:33:12 -0400

    TheVoiceCommit

---
+++ Assets/Scripts/ActivateVoice.cs
@@ -0,0 +1,67 @@
+using Meta.WitAi;
+using Meta.WitAi.Requests;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ActivateVoice : MonoBehaviour
+{
+    // The button label to be adjusted with state
+    [SerializeField] private TMP_Text _buttonLabel;
+
+    [Tooltip("Text to be shown while the voice service is not active")]
+    [SerializeField] private string _activateText = "Activate";
+
+    [Tooltip("Reference to the current voice service")]
+    [SerializeField] private VoiceService _voiceService;
+
+    [Tooltip("Text to be shown while the voice service is active")]
+    [SerializeField] private string _deactivateText = "Deactivate";
+
+    private VoiceServiceRequest _request;
+    void Awake()
+    {
+        if (_voiceService == null)
+        {
+            _voiceService = FindObjectOfType<VoiceService>();
+        }
+    }
+    public void Activate()
+    {
+        _request = _voiceService.ActivateImmediately(GetRequestEvents());
+    }
+
+    public void Deactivate()
+    {
+        _request.DeactivateAudio();
+    }
+    // Get events
+    private VoiceServiceRequestEvents GetRequestEvents()
+    {
+        VoiceServiceRequestEvents events = new VoiceServiceRequestEvents();
+        events.OnInit.AddListener(OnInit);
+        events.OnComplete.AddListener(OnComplete);
+        return events;
+    }
+    // Request initialized
+    private void OnInit(VoiceServiceRequest request)
+    {
+        RefreshActive(true);
+    }
+    // Request completed
+    private void OnComplete(VoiceServiceRequest request)
+    {
+        RefreshActive(false);
+    }
+
+    // Refresh active text
+    private void RefreshActive(bool _isActive)
+    {
+        if (_buttonLabel != null)
+        {
+            _buttonLabel.text = _isActive ? _deactivateText : _activateText;
+        }
+    }
+}
