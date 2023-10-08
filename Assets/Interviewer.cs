b6626793271129cb07f3b3ac862ea8ae42f11466
superpatrex <jack@ding911.com>
2023-10-07 22:34:12 -0400

    Added AI and jazz. In addition I fixed the .gitignore
    Yeet Jack is so cool

--- Assets/Interviewer.cs
+++ Assets/Interviewer.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using Unity.VisualScripting;
 using Unity.XR.Oculus;
 using UnityEngine;
